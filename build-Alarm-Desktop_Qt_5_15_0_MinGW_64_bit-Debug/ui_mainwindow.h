aee8c91bae9e241946f3c59dc5f6dc3a823f6095
klaxonsdev <faiiiz69@gmail.com>
2020-07-23 12:53:28 +0200

    ui

--- build-Alarm-Desktop_Qt_5_15_0_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-Alarm-Desktop_Qt_5_15_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -13,11 +13,13 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QCalendarWidget>
 #include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLCDNumber>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QListWidget>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTabWidget>
 #include <QtWidgets/QTextEdit>
@@ -50,6 +52,46 @@ public:
     QPushButton *pushButton_4;
     QPushButton *pushButton_3;
     QPushButton *removebutton;
+    QWidget *tab;
+    QGridLayout *gridLayout_3;
+    QLabel *label_30;
+    QLabel *label_11;
+    QLabel *label_12;
+    QLCDNumber *lcdhours;
+    QLCDNumber *lcdmin;
+    QLCDNumber *lcdsecc;
+    QLabel *label_8;
+    QLabel *label_7;
+    QLabel *label_10;
+    QPushButton *pushButtonalex;
+    QSpinBox *spinBox_5;
+    QSpinBox *spinBox;
+    QSpinBox *spinBox_2;
+    QPushButton *pushButtonalex_5;
+    QWidget *tab_3;
+    QGridLayout *gridLayout_4;
+    QLabel *hoursLabel_3;
+    QLabel *minutesLabel_3;
+    QLabel *secondsLabel_3;
+    QLCDNumber *hourScreen_3;
+    QLCDNumber *minuteScreen_3;
+    QLCDNumber *secondScreen_3;
+    QPushButton *startButton_3;
+    QPushButton *stopButton_3;
+    QWidget *tab_4;
+    QGridLayout *gridLayout_5;
+    QLabel *label_55;
+    QLabel *label_54;
+    QLCDNumber *lcdNumber_28;
+    QLCDNumber *lcdNumber_31;
+    QLCDNumber *lcdNumber_30;
+    QLCDNumber *lcdNumber_32;
+    QLCDNumber *lcdNumber_29;
+    QLCDNumber *lcdNumber_33;
+    QLabel *label_56;
+    QLCDNumber *lcdNumber_34;
+    QLCDNumber *lcdNumber_36;
+    QLCDNumber *lcdNumber_35;
     QCalendarWidget *calendarWidget;
     QMenuBar *menubar;
     QStatusBar *statusbar;
@@ -76,6 +118,10 @@ public:
         sizePolicy.setHeightForWidth(tabWidget->sizePolicy().hasHeightForWidth());
         tabWidget->setSizePolicy(sizePolicy);
         tabWidget->setMinimumSize(QSize(364, 196));
+        QFont font;
+        font.setBold(false);
+        font.setWeight(50);
+        tabWidget->setFont(font);
         Wetter = new QWidget();
         Wetter->setObjectName(QString::fromUtf8("Wetter"));
         gridLayout_2 = new QGridLayout(Wetter);
@@ -100,11 +146,11 @@ public:
         label_2->setObjectName(QString::fromUtf8("label_2"));
         sizePolicy.setHeightForWidth(label_2->sizePolicy().hasHeightForWidth());
         label_2->setSizePolicy(sizePolicy);
-        QFont font;
-        font.setPointSize(26);
-        font.setBold(true);
-        font.setWeight(75);
-        label_2->setFont(font);
+        QFont font1;
+        font1.setPointSize(26);
+        font1.setBold(true);
+        font1.setWeight(75);
+        label_2->setFont(font1);
         label_2->setScaledContents(false);
         label_2->setAlignment(Qt::AlignCenter);
         label_2->setWordWrap(false);
@@ -183,6 +229,214 @@ public:
         gridLayout->addWidget(removebutton, 3, 0, 1, 2);
 
         tabWidget->addTab(tab_2, QString());
+        tab = new QWidget();
+        tab->setObjectName(QString::fromUtf8("tab"));
+        gridLayout_3 = new QGridLayout(tab);
+        gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
+        label_30 = new QLabel(tab);
+        label_30->setObjectName(QString::fromUtf8("label_30"));
+
+        gridLayout_3->addWidget(label_30, 0, 2, 1, 1);
+
+        label_11 = new QLabel(tab);
+        label_11->setObjectName(QString::fromUtf8("label_11"));
+
+        gridLayout_3->addWidget(label_11, 0, 3, 1, 1);
+
+        label_12 = new QLabel(tab);
+        label_12->setObjectName(QString::fromUtf8("label_12"));
+
+        gridLayout_3->addWidget(label_12, 0, 4, 1, 1);
+
+        lcdhours = new QLCDNumber(tab);
+        lcdhours->setObjectName(QString::fromUtf8("lcdhours"));
+
+        gridLayout_3->addWidget(lcdhours, 1, 2, 1, 1);
+
+        lcdmin = new QLCDNumber(tab);
+        lcdmin->setObjectName(QString::fromUtf8("lcdmin"));
+
+        gridLayout_3->addWidget(lcdmin, 1, 3, 1, 1);
+
+        lcdsecc = new QLCDNumber(tab);
+        lcdsecc->setObjectName(QString::fromUtf8("lcdsecc"));
+
+        gridLayout_3->addWidget(lcdsecc, 1, 4, 1, 1);
+
+        label_8 = new QLabel(tab);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+
+        gridLayout_3->addWidget(label_8, 2, 0, 1, 1);
+
+        label_7 = new QLabel(tab);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+
+        gridLayout_3->addWidget(label_7, 2, 1, 1, 1);
+
+        label_10 = new QLabel(tab);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+
+        gridLayout_3->addWidget(label_10, 2, 2, 1, 1);
+
+        pushButtonalex = new QPushButton(tab);
+        pushButtonalex->setObjectName(QString::fromUtf8("pushButtonalex"));
+
+        gridLayout_3->addWidget(pushButtonalex, 2, 3, 1, 2);
+
+        spinBox_5 = new QSpinBox(tab);
+        spinBox_5->setObjectName(QString::fromUtf8("spinBox_5"));
+
+        gridLayout_3->addWidget(spinBox_5, 3, 0, 1, 1);
+
+        spinBox = new QSpinBox(tab);
+        spinBox->setObjectName(QString::fromUtf8("spinBox"));
+
+        gridLayout_3->addWidget(spinBox, 3, 1, 1, 1);
+
+        spinBox_2 = new QSpinBox(tab);
+        spinBox_2->setObjectName(QString::fromUtf8("spinBox_2"));
+        spinBox_2->setButtonSymbols(QAbstractSpinBox::UpDownArrows);
+
+        gridLayout_3->addWidget(spinBox_2, 3, 2, 1, 1);
+
+        pushButtonalex_5 = new QPushButton(tab);
+        pushButtonalex_5->setObjectName(QString::fromUtf8("pushButtonalex_5"));
+
+        gridLayout_3->addWidget(pushButtonalex_5, 3, 3, 1, 2);
+
+        tabWidget->addTab(tab, QString());
+        tab_3 = new QWidget();
+        tab_3->setObjectName(QString::fromUtf8("tab_3"));
+        gridLayout_4 = new QGridLayout(tab_3);
+        gridLayout_4->setObjectName(QString::fromUtf8("gridLayout_4"));
+        hoursLabel_3 = new QLabel(tab_3);
+        hoursLabel_3->setObjectName(QString::fromUtf8("hoursLabel_3"));
+
+        gridLayout_4->addWidget(hoursLabel_3, 0, 0, 1, 1);
+
+        minutesLabel_3 = new QLabel(tab_3);
+        minutesLabel_3->setObjectName(QString::fromUtf8("minutesLabel_3"));
+
+        gridLayout_4->addWidget(minutesLabel_3, 0, 1, 1, 1);
+
+        secondsLabel_3 = new QLabel(tab_3);
+        secondsLabel_3->setObjectName(QString::fromUtf8("secondsLabel_3"));
+
+        gridLayout_4->addWidget(secondsLabel_3, 0, 2, 1, 1);
+
+        hourScreen_3 = new QLCDNumber(tab_3);
+        hourScreen_3->setObjectName(QString::fromUtf8("hourScreen_3"));
+
+        gridLayout_4->addWidget(hourScreen_3, 1, 0, 1, 1);
+
+        minuteScreen_3 = new QLCDNumber(tab_3);
+        minuteScreen_3->setObjectName(QString::fromUtf8("minuteScreen_3"));
+
+        gridLayout_4->addWidget(minuteScreen_3, 1, 1, 1, 1);
+
+        secondScreen_3 = new QLCDNumber(tab_3);
+        secondScreen_3->setObjectName(QString::fromUtf8("secondScreen_3"));
+
+        gridLayout_4->addWidget(secondScreen_3, 1, 2, 1, 1);
+
+        startButton_3 = new QPushButton(tab_3);
+        startButton_3->setObjectName(QString::fromUtf8("startButton_3"));
+
+        gridLayout_4->addWidget(startButton_3, 2, 0, 1, 1);
+
+        stopButton_3 = new QPushButton(tab_3);
+        stopButton_3->setObjectName(QString::fromUtf8("stopButton_3"));
+
+        gridLayout_4->addWidget(stopButton_3, 2, 1, 1, 2);
+
+        tabWidget->addTab(tab_3, QString());
+        tab_4 = new QWidget();
+        tab_4->setObjectName(QString::fromUtf8("tab_4"));
+        gridLayout_5 = new QGridLayout(tab_4);
+        gridLayout_5->setObjectName(QString::fromUtf8("gridLayout_5"));
+        label_55 = new QLabel(tab_4);
+        label_55->setObjectName(QString::fromUtf8("label_55"));
+
+        gridLayout_5->addWidget(label_55, 0, 0, 1, 2);
+
+        label_54 = new QLabel(tab_4);
+        label_54->setObjectName(QString::fromUtf8("label_54"));
+
+        gridLayout_5->addWidget(label_54, 0, 3, 1, 2);
+
+        lcdNumber_28 = new QLCDNumber(tab_4);
+        lcdNumber_28->setObjectName(QString::fromUtf8("lcdNumber_28"));
+
+        gridLayout_5->addWidget(lcdNumber_28, 1, 0, 1, 1);
+
+        lcdNumber_31 = new QLCDNumber(tab_4);
+        lcdNumber_31->setObjectName(QString::fromUtf8("lcdNumber_31"));
+
+        gridLayout_5->addWidget(lcdNumber_31, 1, 1, 1, 1);
+
+        lcdNumber_30 = new QLCDNumber(tab_4);
+        lcdNumber_30->setObjectName(QString::fromUtf8("lcdNumber_30"));
+
+        gridLayout_5->addWidget(lcdNumber_30, 1, 2, 1, 1);
+
+        lcdNumber_32 = new QLCDNumber(tab_4);
+        lcdNumber_32->setObjectName(QString::fromUtf8("lcdNumber_32"));
+        lcdNumber_32->setAutoFillBackground(false);
+        lcdNumber_32->setStyleSheet(QString::fromUtf8("font: 8pt \"MS Shell Dlg 2\";\n"
+"font-color: rgb(0, 0, 0);"));
+
+        gridLayout_5->addWidget(lcdNumber_32, 1, 3, 1, 1);
+
+        lcdNumber_29 = new QLCDNumber(tab_4);
+        lcdNumber_29->setObjectName(QString::fromUtf8("lcdNumber_29"));
+        QPalette palette;
+        QBrush brush(QColor(0, 0, 0, 255));
+        brush.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Button, brush);
+        palette.setBrush(QPalette::Active, QPalette::Midlight, brush);
+        QBrush brush1(QColor(255, 255, 255, 255));
+        brush1.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Base, brush1);
+        palette.setBrush(QPalette::Active, QPalette::Window, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::Button, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::Midlight, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::Base, brush1);
+        palette.setBrush(QPalette::Inactive, QPalette::Window, brush);
+        palette.setBrush(QPalette::Disabled, QPalette::Button, brush);
+        palette.setBrush(QPalette::Disabled, QPalette::Midlight, brush);
+        palette.setBrush(QPalette::Disabled, QPalette::Base, brush);
+        palette.setBrush(QPalette::Disabled, QPalette::Window, brush);
+        lcdNumber_29->setPalette(palette);
+        lcdNumber_29->setSmallDecimalPoint(false);
+
+        gridLayout_5->addWidget(lcdNumber_29, 1, 4, 1, 1);
+
+        lcdNumber_33 = new QLCDNumber(tab_4);
+        lcdNumber_33->setObjectName(QString::fromUtf8("lcdNumber_33"));
+
+        gridLayout_5->addWidget(lcdNumber_33, 1, 5, 1, 1);
+
+        label_56 = new QLabel(tab_4);
+        label_56->setObjectName(QString::fromUtf8("label_56"));
+
+        gridLayout_5->addWidget(label_56, 2, 0, 1, 2);
+
+        lcdNumber_34 = new QLCDNumber(tab_4);
+        lcdNumber_34->setObjectName(QString::fromUtf8("lcdNumber_34"));
+
+        gridLayout_5->addWidget(lcdNumber_34, 3, 0, 1, 1);
+
+        lcdNumber_36 = new QLCDNumber(tab_4);
+        lcdNumber_36->setObjectName(QString::fromUtf8("lcdNumber_36"));
+
+        gridLayout_5->addWidget(lcdNumber_36, 3, 1, 1, 1);
+
+        lcdNumber_35 = new QLCDNumber(tab_4);
+        lcdNumber_35->setObjectName(QString::fromUtf8("lcdNumber_35"));
+
+        gridLayout_5->addWidget(lcdNumber_35, 3, 2, 1, 1);
+
+        tabWidget->addTab(tab_4, QString());
 
         verticalLayout->addWidget(tabWidget);
 
@@ -225,6 +479,25 @@ public:
         pushButton_3->setText(QCoreApplication::translate("MainWindow", "Schlummer testen", nullptr));
         removebutton->setText(QCoreApplication::translate("MainWindow", "Entfernen", nullptr));
         tabWidget->setTabText(tabWidget->indexOf(tab_2), QCoreApplication::translate("MainWindow", "Alarm", nullptr));
+        label_30->setText(QCoreApplication::translate("MainWindow", "Hours:", nullptr));
+        label_11->setText(QCoreApplication::translate("MainWindow", "Minutes:", nullptr));
+        label_12->setText(QCoreApplication::translate("MainWindow", "Seconds:", nullptr));
+        label_8->setText(QCoreApplication::translate("MainWindow", "Hours:", nullptr));
+        label_7->setText(QCoreApplication::translate("MainWindow", "Minutes:", nullptr));
+        label_10->setText(QCoreApplication::translate("MainWindow", "Seconds:", nullptr));
+        pushButtonalex->setText(QCoreApplication::translate("MainWindow", "start", nullptr));
+        pushButtonalex_5->setText(QCoreApplication::translate("MainWindow", "stop", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QCoreApplication::translate("MainWindow", "Timer", nullptr));
+        hoursLabel_3->setText(QCoreApplication::translate("MainWindow", "Hours", nullptr));
+        minutesLabel_3->setText(QCoreApplication::translate("MainWindow", "Minutes", nullptr));
+        secondsLabel_3->setText(QCoreApplication::translate("MainWindow", "Seconds", nullptr));
+        startButton_3->setText(QCoreApplication::translate("MainWindow", "start/pause", nullptr));
+        stopButton_3->setText(QCoreApplication::translate("MainWindow", "stop", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_3), QCoreApplication::translate("MainWindow", "Stoppuhr", nullptr));
+        label_55->setText(QCoreApplication::translate("MainWindow", "Berlin", nullptr));
+        label_54->setText(QCoreApplication::translate("MainWindow", "Los Angeles", nullptr));
+        label_56->setText(QCoreApplication::translate("MainWindow", "Moskau", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_4), QCoreApplication::translate("MainWindow", "Weltuhr", nullptr));
     } // retranslateUi
 
 };
